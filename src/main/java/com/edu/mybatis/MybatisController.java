cde23a9172b82196371914330f75f15895fad33a
hanga9653 <rhtqoa1@naver.com>
2023-02-01 17:33:41 +0900

    2-1수업

---
+++ src/main/java/com/edu/mybatis/MybatisController.java
@@ -0,0 +1,22 @@
+package com.edu.mybatis;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MybatisController {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping("/Mybatis/list.do")
+	public String list(Model model, HttpServletRequest req) {
+		return "07Mybatis/list";
+	}
+}
